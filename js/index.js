ef3c78cf2d5315a85651ccb3c18b4ccb2c4ff44a
AWSafran <andrewsafran@gmail.com>
2019-03-05 15:19:49 -0500

    added nested event listeners with stopPropagation method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//example of .stopPropagation()
+const welcomeMessage = document.querySelector("h2");
+welcomeMessage.addEventListener('click', function(event){
+    welcomeMessage.textContent = "Hello!";
+    event.stopPropagation();
+});
+
+const pageHeader = document.querySelector(".intro");
+pageHeader.addEventListener('click', function(){
+    pageHeader.style.color = "red";
+});
